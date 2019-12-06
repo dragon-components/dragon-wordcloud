5a04f3f1d3672dd094238916a56f96d07ab3bb64
张金龙 <zhangjinlong@znlhzl.com>
2019-12-06 15:50:17 +0800

    add: 请求封装

--- src/demo.js
+++ src/demo.js
@@ -1,6 +1,27 @@
 import './styles.less'
 import initCloud from './index'
 import data from './nice.json'
+import axios from 'axios'
+// 文字库
+function getText() {
+  return axios.get('/user/12345');
+}
+// 图片库
+function getImage() {
+  return axios.get('/user/12345/permissions');
+}
+// TODO
+/*axios.all([getText(), getImage()])
+  .then(function(response) {
+    let data = response[0]
+    let imageUrl = response[1]
+    const chart = initCloud(document.getElementById('demo'), data, {imageUrl: imageUrl})
+    chart.on('click', ({
+      data
+    }) => {
+      alert(data.name)
+    })
+})*/
 const chart = initCloud(document.getElementById('demo'), data, {imageUrl: ''})
 
 console.log(chart)
